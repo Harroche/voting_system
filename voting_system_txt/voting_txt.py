56b7077cab9934619c490b95c90ac20cafee6f38
Harroche <119439246+Harroche@users.noreply.github.com>
2023-05-12 18:35:16 +0100

    got rid of old comments and added input amount of seats in election

--- voting_system_txt/voting_txt.py
+++ voting_system_txt/voting_txt.py
@@ -1,36 +1,11 @@
 import pyrankvote
 from pyrankvote import Candidate, Ballot
 
-# #opens file 
-# file = open("STV/votes.txt","r")
-# lineForCan = file.readline().replace("\n","")
-# canArr= lineForCan.split('\t') #places candiates in array
-# candidates =[]
-# canDict ={}
-# count =0
-# #puts in both an array and dict
-# for can in canArr:
-#     candidates.append(Candidate(can))
-#     canDict.update({canArr[count]: Candidate(can)})
-#     count+=1
- 
-# lines = file.readlines()
-# ballot=[]
-# #puts all the votes in the ballot 
-# ballot.append(Ballot(ranked_candidates=[canDict.get(canArr[0]),canDict.get(canArr[1]),canDict.get(canArr[2])])) #first  line
-# for line in lines:  #the rest of the lines
-#     line.replace("\n","")
-#     standing=line.split()
-#     ballot.append(Ballot(ranked_candidates=[Candidate(standing[0]),Candidate(standing[1]),Candidate(standing[2])]))
-
-# #prints election results 
-# election_results = pyrankvote.single_transferable_vote(candidates,ballot,1)
-# print(election_results)
-#opens file 
-file = open("STV/votes.txt","r")
+file = open("voting_system_txt/votes.txt","r")
 lineForCan = file.readline().replace("\n","")
 canArr= lineForCan.split('\t') #places candiates in array
 candidates = []
+seats=int(input("input the amount of seats for postion: "))
 #puts in both an array and dict
 for can in canArr:
     candidates.append(Candidate(can))
@@ -46,5 +21,5 @@ for line in lines:  #the rest of the lines
     ballot.append(Ballot(ranked_candidates=ballotArray))
 
 #prints election results 
-election_results = pyrankvote.single_transferable_vote(candidates,ballot,1)
+election_results = pyrankvote.single_transferable_vote(candidates,ballot,seats)
 print(election_results)
